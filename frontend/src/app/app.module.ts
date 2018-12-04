2a76fd6c4465877543ee8b37922da59859fe06b4
ababacarkamara <ababacarkamara@gmail.com>
2018-12-04 10:45:03 +0000

    matToolbar

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,8 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {RouterModule, Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 
-import { AppRoutingModule } from './app-routing.module';
+import { MatToolbarModule } from '@angular/material';
+
+// import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ListComponent } from './components/list/list.component';
@@ -10,10 +12,10 @@ import { CreateComponent } from './components/create/create.component';
 import { EditComponent } from './components/edit/edit.component';
 
 const routes: Routes = [
-  {path: 'create' , component: CreateComponent},
-  {path: 'edit/id' , component: EditComponent},
-  {path: 'list' , component: ListComponent},
-  {path: '' , redirectTo: 'list', pathMatch: 'full'}
+  { path: 'create' , component: CreateComponent },
+  { path: 'edit/id' , component: EditComponent },
+  { path: 'list' , component: ListComponent },
+  { path: '' , redirectTo: 'list', pathMatch: 'full' }
 ];
 @NgModule({
   declarations: [
@@ -26,7 +28,8 @@ const routes: Routes = [
     BrowserModule,
     // AppRoutingModule,
     BrowserAnimationsModule,
-    RouterModule.forRoot(routes)
+    RouterModule.forRoot(routes),
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
