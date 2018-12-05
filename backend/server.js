374d2cc4a02b5c1fbc9cd118f2d4f9345f5bed0c
ababacarkamara <ababacarkamara@gmail.com>
2018-12-05 18:03:15 +0000

    mongoose

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ const router = express.Router();
 app.use(cors());
 app.use(bodyParser.json());
 
-mongoose.connect('mongodb://localhost:27017/');
+mongoose.connect('mongodb://localhost:27017/eleves');
 
 const connection = mongoose.connection;
 
